4ab955ddf50022a2f2da6942a62fd3c87d40b57e
linuxu2015 <linuxu2015@gmail.com>
2016-06-07 21:41:02 +0800

    设置自动获取cookies

--- headers.py
+++ headers.py
@@ -5,7 +5,6 @@ get_ptwebqq =  {
 'Cache-Control':'max-age=0',
 'Connection':'keep-alive',
 'Content-Type':'utf-8',
-'Cookie':'RK=KclzAe73YJ; ts_refer=www.baidu.com/link; hm_pay=h010b117a699ed0e179fa7e3f01b52fad7e20d59f917d7dd4e6b58b4bb3956b3179c73474151d6c3e6c; defaultMonth=3; ptcz=a38d369f1f5e9baf03e343a0fc79e87153f99cc289c8da931f47ccaf1845c372; ts_uid=5691127284; ptisp=ctc; pt2gguin=o0371044414; uin=o0371044414; skey=@x7iOvoc5E; p_uin=o0371044414; p_skey=OyvBGIZaxtr4UcVa9NById-enQaADMMnw*vdfA2ZuaU_; pt4_token=YRkL4hikWcN2ftYzEuDFGgcWMVa1tdkduvCE6JUzDk4_; pgv_info=ssid=s5857382185; pgv_pvid=9827197976; o_cookie=371044414; ptwebqq=d68fcef50716c3fa75c216de0b9a0b08ee83c989d19dee336b0421e74a671caf',
 'Host':'s.web2.qq.com',
 'Referer':'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
@@ -17,7 +16,6 @@ get_psessionid = {
 'Connection':'keep-alive',
 'Content-Length':'180',
 'Content-Type':'application/x-www-form-urlencoded',
-'Cookie':'RK=KclzAe73YJ; ts_refer=www.baidu.com/link; hm_pay=h010b117a699ed0e179fa7e3f01b52fad7e20d59f917d7dd4e6b58b4bb3956b3179c73474151d6c3e6c; defaultMonth=3; ptcz=a38d369f1f5e9baf03e343a0fc79e87153f99cc289c8da931f47ccaf1845c372; ts_uid=5691127284; ptisp=ctc; pt2gguin=o0371044414; uin=o0371044414; skey=@x7iOvoc5E; p_uin=o0371044414; p_skey=OyvBGIZaxtr4UcVa9NById-enQaADMMnw*vdfA2ZuaU_; pt4_token=YRkL4hikWcN2ftYzEuDFGgcWMVa1tdkduvCE6JUzDk4_; pgv_info=ssid=s5857382185; pgv_pvid=9827197976; o_cookie=371044414; ptwebqq=d68fcef50716c3fa75c216de0b9a0b08ee83c989d19dee336b0421e74a671caf',
 'Host':'d1.web2.qq.com',
 'Origin':'http://d1.web2.qq.com',
 'Referer':'http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2',
@@ -30,7 +28,6 @@ send_msg = {
 'Connection':'keep-alive',
 'Content-Length':'601',
 'Content-Type':'application/x-www-form-urlencoded',
-'Cookie':'RK=KclzAe73YJ; ts_refer=www.baidu.com/link; hm_pay=h010b117a699ed0e179fa7e3f01b52fad7e20d59f917d7dd4e6b58b4bb3956b3179c73474151d6c3e6c; defaultMonth=3; ptcz=a38d369f1f5e9baf03e343a0fc79e87153f99cc289c8da931f47ccaf1845c372; ts_uid=5691127284; ptisp=ctc; pt2gguin=o0371044414; uin=o0371044414; skey=@x7iOvoc5E; p_uin=o0371044414; p_skey=OyvBGIZaxtr4UcVa9NById-enQaADMMnw*vdfA2ZuaU_; pt4_token=YRkL4hikWcN2ftYzEuDFGgcWMVa1tdkduvCE6JUzDk4_; pgv_info=ssid=s5857382185; pgv_pvid=9827197976; o_cookie=371044414; ptwebqq=d68fcef50716c3fa75c216de0b9a0b08ee83c989d19dee336b0421e74a671caf',
 'Host':'d1.web2.qq.com',
 'Origin':'http://d1.web2.qq.com',
 'Referer':'http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2',
--- main.py
+++ main.py
@@ -9,54 +9,87 @@ import random
 reload(sys)
 sys.setdefaultencoding('utf-8')
 def saveImage(imgName = 'default.jpg'):
+    session = requests.Session()
     imgurl = 'https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t=0.3835296393318739'
-    response = requests.get(imgurl,stream=True)
+    response = session.get(imgurl,stream=True)
+    cookies = session.cookies
+    print cookies
     print response
     image = response.content
-    #DstDir = "F:\\image\\"
-    DstDir = "/opt/qqapi/img/"
+    DstDir = "E:\\qqapi\\img\\"
+    #DstDir = "/opt/qqapi/img/"
     print "save"
     with open(DstDir+imgName ,"wb") as jpg:
         jpg.write(image)
-        return
+       # return
     jpg.close
-#saveImage()
-
+    return cookies
+cookies = saveImage()
+print cookies
+time.sleep(10)
 session = requests.Session()
-session.headers = headers.get_ptwebqq
-ti = int(time.time()*1000)
-url = 'http://s.web2.qq.com/api/getvfwebqq?ptwebqq=d68fcef50716c3fa75c216de0b9a0b08ee83c989d19dee336b0421e74a671caf&clientid=53999199&psessionid=&t=%s' %ti
+verify_erwei = 'https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=501004106%20&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10%20&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert%20&action=0-0-157510&mibao_css=m_webqq&t=1&g=1&js_type=0&js_ver=10143&login_sig=&pt_randsalt=0'
+res = session.get(verify_erwei,cookies=cookies)
+data = str(res.text)
+print data
+str_url = data.split(',')[2]
+url = str_url.strip("'")
+print url
+'''
+ptuiCB('0','0',
+'http://ptlogin4.web2.qq.com/check_sig?pttype=1&uin=371044414&service=ptqrlogin&nodirect=0&ptsigx=565553cbc69d8e4fc89170aa0b323c2297d5a5ffc4cd428956ef9be0f2b23f6069b410aeda438d123318f236fa5c561263367e3f98b1c1a2698f3c0075b1bfce&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10%20&f_url=&ptlang=2052&ptredirect=100&aid=501004106&daid=164&j_later=0&low_login_hour=0&regmaster=0&pt_login_type=3&pt_aid=0&pt_aaid=16&pt_light=0&pt_3rd_aid=0
+','0','登录成功！', '那颗星');
+
+'''
+
+##获取ptwebqq
 res = session.get(url)
+#print res.text
+new_cookies = session.cookies
+for item in  new_cookies:
+	if item.name == 'ptwebqq':
+		ptwebqq = item.value
+print ptwebqq
+
+session.headers = headers.get_ptwebqq
+
+ti = int(time.time()*1000)
+url = 'http://s.web2.qq.com/api/getvfwebqq?ptwebqq=%s&clientid=53999199&psessionid=&t=%s' %(ptwebqq,ti)
+res = session.get(url,cookies=new_cookies)
+
 data = json.loads(res.text)
+#print data
 vfwebqq = data['result']['vfwebqq']
-#print vfwebqq
+print vfwebqq
 session = requests.Session()
 session.headers = headers.get_psessionid
 data ={
- "ptwebqq": "d68fcef50716c3fa75c216de0b9a0b08ee83c989d19dee336b0421e74a671caf",
+ "ptwebqq": ptwebqq,
   "clientid": 53999199,
   "psessionid": "",
   "status": "online",
 }
 r = json.dumps(data)
-a = session.post('http://d1.web2.qq.com/channel/login2',data={"r":r})
+a = session.post('http://d1.web2.qq.com/channel/login2',cookies=new_cookies,data={"r":r})
 data = json.loads(a.text)
+print data
 psessionid = data['result']['psessionid']
-#print psessionid
+print psessionid
 session = requests.Session()
 session.headers = headers.send_msg
 status = True
 while status:
     msg_id = int(random.random()*100000000)
     msg = {
-    "to":3541204146,
-    "content":"[\"邵永……\",[\"font\",{\"name\":\"宋体\",\"size\":10,\"style\":[0,0,0],\"color\":\"000000\"}]]",
+    "to":371044414,
+    "content":"[\"test……\",[\"font\",{\"name\":\"宋体\",\"size\":10,\"style\":[0,0,0],\"color\":\"000000\"}]]",
     "face":549,
     "clientid":53999199,
     "msg_id":msg_id,
     "psessionid":psessionid
     }
-    a = session.post('http://d1.web2.qq.com/channel/send_buddy_msg2',data = {"r":json.dumps(msg)})
+    a = session.post('http://d1.web2.qq.com/channel/send_buddy_msg2',cookies=new_cookies,data = {"r":json.dumps(msg)})
+    print a.text
     if "ok" in a.text:
         status = False
         print "send sussess"
@@ -65,4 +98,3 @@ while status:
         status = True
         print "send failed"
         time.sleep(5)
-     
